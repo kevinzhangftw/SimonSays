68e2ee18aeb0d6f3eb16e550ebdef57bc0948f30
Kevin Zhang <kevinzhang@Kevins-MacBook-Air.local>
2015-03-04 20:04:37 -0800

    Update after depressing button got really drepresses and stop working

---
+++ SimonSays copy/SimonColorButton.swift
@@ -0,0 +1,14 @@
+//
+//  SimonColorButton.swift
+//  SimonSays
+//
+//  Created by Kevin Zhang on 2015-03-03.
+//  Copyright (c) 2015 Kevin Zhang. All rights reserved.
+//
+
+import UIkit
+
+class SimonColorButton: UIButton {
+    
+//TODO: make it flash
+}
